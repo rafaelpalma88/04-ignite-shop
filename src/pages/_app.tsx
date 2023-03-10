6a169f06622db18033ede6e090a69242933599fd
Rafael Costa Palma <rafaelcostapalma@protonmail.com>
2023-03-10 09:40:22 -0300

    adding public key

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,22 @@
-import '@/styles/globals.css'
+import { globalStyles } from '../styles/global'
 import type { AppProps } from 'next/app'
+import logoImg from '../assets/logo.svg'
+import { Container, Header } from '../styles/pages/app'
+import Image from 'next/image'
+
+globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Container>
+      <Header>
+        <Image src={logoImg.src} width="130" height="52" alt="" />
+      </Header>
+      <Component {...pageProps} />
+    </Container>
+    
+  )
+  
+  
+  
 }
